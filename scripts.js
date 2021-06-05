9bc2b6a5ccabd8a1a10dc11652148e3b08b2e3ef
Alessandro Junior <alessandrojunior@MacBook-Air-de-Alessandro.local>
2021-06-05 15:05:13 -0300

    Adicionado menu de busca

--- scripts.js
+++ scripts.js
@@ -1,14 +1,8 @@
 
-// Get Elements --------------------------------------------
-const container = getElement('.fundo');
-  
+const container = document.querySelector('.fundo');
 
-// Função para reduzir a escrita na captura de elementos HTML
-function getElement(element) {
-  return document.querySelector(element);
-}
 
-// Função responsavel por fazer requisições para a API e inserir as respostas na variavel pokemon
+
 function requestPokeInfo() {
     const getUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
     const PokeArray= [];
@@ -21,12 +15,12 @@ function requestPokeInfo() {
 
             const spanPoke = pokemons.reduce((accumulator,pokemon) => {
                 const types = pokemon.types.map(typeInfo => typeInfo.type.name);
-                console.log(types);
+//                console.log(types);
 
                 accumulator += `
                 
                 
-                <li class="card ${types[0]} ${types[1]}">
+                <li class="card ${types[0]} ${types[1]}" onclick="requestInfo(${pokemon.id})">
                 <div class="fundo">
                 <img class="card-img" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
                 </div>
@@ -37,12 +31,123 @@ function requestPokeInfo() {
                 `
                 return accumulator;
 
+
             },'')
             const ul = document.querySelector('[data-js = "pokedex"]');
             ul.innerHTML = spanPoke;
 
         })
 }
+
+function requestPokeInput(){
+
+    
+    var conteudoInput = document.getElementById('buscar').value;
+
+    if(conteudoInput == ''){
+        requestPokeInfo();
+    }
+    else{
+        const getUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+     const PokeArray= [];
+
+
+         PokeArray.push(fetch(getUrl(conteudoInput)).then(response => response.json()));
+    
+         Promise.all(PokeArray)
+         .then(pokemons => {
+
+
+                 const spanPoke = pokemons.reduce((accumulator,pokemon) => {
+                 const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+
+                 accumulator += `
+                
+                
+                 <li class="card ${types[0]} ${types[1]}">
+                 <div class="fundo">
+                 <img class="card-img" alt="${pokemon.name}" src="https:pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
+                 </div>
+                     <h2>${pokemon.id}.${pokemon.name}</h2>
+                     <p class="card-info">${types.join(' | ')}</p>
+                 </li>
+
+                 <li class="card ${types[0]} ${types[1]}">
+                 <div class="fundo">
+                 </div>
+                 <h2 class="name">Name: ${pokemon.name}</h2>
+                 <h3 class="type">Type: ${pokemon.types.map(item => '' + item.type.name).toString()}</h3>
+                 <h3 class="weight">Weight: ${pokemon.weight  / 10}kg</h3>
+                 <h3 class="height">Height: ${pokemon.height  / 10}m</h3>
+                </li>
+                
+                 `
+                 return accumulator;
+
+
+             },'')
+             const ul = document.querySelector('[data-js = "pokedex"]');
+             ul.innerHTML = spanPoke;
+
+         })
+    console.log(conteudoInput);
+    }
+     
+}
+
+function requestInfo(conteudoBusca){
+
+    
+
+    if(conteudoBusca == ''){
+        requestPokeInfo();
+    }
+    else{
+        const getUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+     const PokeArray= [];
+
+
+         PokeArray.push(fetch(getUrl(conteudoBusca)).then(response => response.json()));
+    
+         Promise.all(PokeArray)
+         .then(pokemons => {
+
+
+                 const spanPoke = pokemons.reduce((accumulator,pokemon) => {
+                 const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+
+                 accumulator += `
+                
+                 <li class="card ${types[0]} ${types[1]}">
+                 <div class="fundo">
+                 <img class="card-img" alt="${pokemon.name}" src="https:pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
+                 </div>
+                     <h2>${pokemon.id}.${pokemon.name}</h2>
+                     <p class="card-info">${types.join(' | ')}</p>
+                 </li>
+
+                 <li class="card ${types[0]} ${types[1]}">
+                 <div class="fundo">
+                 </div>
+                 <h2 class="name">Name: ${pokemon.name}</h2>
+                 <h3 class="type">Type: ${pokemon.types.map(item => '' + item.type.name).toString()}</h3>
+                 <h3 class="weight">Weight: ${pokemon.weight  / 10}kg</h3>
+                 <h3 class="height">Height: ${pokemon.height  / 10}m</h3>
+                </li>
+                
+                 `
+                 return accumulator;
+
+
+             },'')
+             const ul = document.querySelector('[data-js = "pokedex"]');
+             ul.innerHTML = spanPoke;
+
+         })
+    console.log(conteudoBusca);
+    }
+     
+}
+
 requestPokeInfo();
-
-
+requestPokeInput();
